64e44b4ebc909e62770a281fc23004bc4aaa3575
Jesper Svennevid <github@svennevid.se>
2010-11-19 18:25:09 +0100

    Removed old copyright headers

--- Projects/Bp06Demo/Parts/MountainsPart.h
+++ Projects/Bp06Demo/Parts/MountainsPart.h
@@ -22,17 +22,6 @@ SOFTWARE.
 
 */
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// This software is supplied under the terms of a license agreement or
-// nondisclosure agreement and may not be copied or disclosed except in
-// accordance with the terms of that agreement.
-//
-// Copyright (c) 2006 Jesper Svennevid, Daniel Collin.
-// All Rights Reserved.
-//
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #ifndef zenic_ps2_MountainsPart_h
 #define zenic_ps2_MountainsPart_h
 
