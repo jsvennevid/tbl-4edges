8db343035c0b95ee60e5d18aed82d48805b66796
Jesper Svennevid <github@svennevid.se>
2010-11-19 18:18:26 +0100

    Added copyright headers

--- Shared/Graphics/Scene/Bound.h
+++ Shared/Graphics/Scene/Bound.h
@@ -1,13 +1,26 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// This software is supplied under the terms of a license agreement or
-// nondisclosure agreement and may not be copied or disclosed except in
-// accordance with the terms of that agreement.
-//
-// Copyright (c) 2003-2005 Jesper Svennevid, Daniel Collin.
-// All Rights Reserved.
-//
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+Copyright (c) 2004-2006 Jesper Svennevid, Daniel Collin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 #ifndef zenic_Bound_h
 #define zenic_Bound_h
